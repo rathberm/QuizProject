14a064492fd9abe5bea8eb6460db03947bf9647b
OnlyNew <jan-roth1998@web.de>
2019-01-28 14:36:56 +0100

    FileAccess updated - added getCategories & started implementing getQuestionsOfCategories

--- src/FileAccess.java
+++ src/FileAccess.java
@@ -1,16 +1,61 @@
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileAccess {
+    private String folderPath;
 
-    public FileAccess(String folderPath){
-        System.out.println(System.getProperty("user.home"));
+    /**
+     * Standardkonstruktor
+     */
+    public FileAccess(){
+        folderPath = System.getProperty("user.home") + "\\AppData\\Roaming\\QuizProject";
+        getCategories();
     }
 
+    /**
+     * Konstruktor, bei welchem ein anderer Dateispeicherort gewählt werden kann
+     * @param folderPath neuer Dateispeicherort
+     */
+    public FileAccess(String pFolderPath){
+        folderPath = pFolderPath;
+    }
+
+    /**
+     * Gibt eine Liste der bereits existierenden / verfügbaren Kategorien zurück
+     * @return liste an zu wählenden Kategorien
+     */
     public String[] getCategories(){
-        return null;
+        File[] files = getFilesOfFolder();
+        String[] categories = new String[files.length];
+
+        for (int i = 0; i < files.length; i++){
+            if (files[i].getName().contains(".")){
+                categories[i] = files[i].getName().replaceFirst("[.][^.]+$", "");
+            } else {
+                categories[i] = files[i].getName();
+            }
+        }
+
+        return categories;
     }
 
-    public Question getQuestionsOfCategorie(){
+    public Question getQuestionsOfCategorie(String pCategorie){
+        File file = new File("");
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                // process the line.
+            }
+            // line is not visible here.
+        } catch(Exception e){
+            System.out.println("Error in: FileAccess.getQuestionsOfCategorie");
+            System.exit(0);
+            return null;
+        }
+
         return null;
     }
 
@@ -18,13 +63,18 @@ public class FileAccess {
 
     }
 
-    public void listFilesForFolder(File folder) {
-        for (File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
-            } else {
-                System.out.println(fileEntry.getName());
-            }
+    /**
+     * Gibt eine Liste der im Ordner befindlichen Dateien zurück
+     * @return Array der Dateinamen
+     */
+    public File[] getFilesOfFolder() {
+        File folder = new File(folderPath);
+        if (folder.isDirectory()){
+            return folder.listFiles();
+        } else {
+            System.out.println("Error in: FileAccess.ListFilesInFolder");
+            System.exit(0);
+            return null;
         }
     }
 }
