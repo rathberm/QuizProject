3e7db20ca5014a6a96b29e98d8a64e9e7fda394b
OnlyNew <jan-roth1998@web.de>
2019-01-28 13:35:44 +0100

    FileAccess updated

--- src/FileAccess.java
+++ src/FileAccess.java
@@ -1,4 +1,30 @@
+import java.io.File;
+
 public class FileAccess {
 
+    public FileAccess(String folderPath){
+        System.out.println(System.getProperty("user.home"));
+    }
 
+    public String[] getCategories(){
+        return null;
+    }
+
+    public Question getQuestionsOfCategorie(){
+        return null;
+    }
+
+    public void createQuestion(){
+
+    }
+
+    public void listFilesForFolder(File folder) {
+        for (File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getName());
+            }
+        }
+    }
 }
